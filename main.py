23d75e2c45e3e6c4b03664e858c86302f472ecb9
Miguel Alzate <alzatemiguel284@gmail.com>
2024-09-15 23:13:13 -0500

    Deleted test route

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.post("/arr")
-async def arr(req: Request):
-    body = await req.json()
-    return body
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
